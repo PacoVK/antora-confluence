5726fc2079ae06d24b4b6defc1959e10c706a6f2
Pascal Euhus <pascal.euhus@gmx.de>
2024-03-11 20:01:37 +0100

    add client tests

---
+++ test/client/ConfluenceClient.test.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it } from "@jest/globals";
+import ConfluenceClientStub from "./ConfluenceClientStub";
+
+describe("ConfluenceClient", () => {
+  const config = {
+    spaceKey: "SPACE",
+    ancestorId: "12345",
+    editorVersion: "v1",
+  };
+
+  it("should initialize the confluence client correctly with full API path", () => {
+    const confluenceClient = new ConfluenceClientStub({
+      ...config,
+      baseUrl: new URL("https://confluence.example.com/rest/api"),
+    });
+    expect(confluenceClient.BASE_URL).toBe("https://confluence.example.com");
+    expect(confluenceClient.API_V1_PATH).toBe("/rest/api");
+    expect(confluenceClient.API_V2_PATH).toBe("/api/v2");
+  });
+
+  it("should initialize the confluence client correctly with default context", () => {
+    const confluenceClient = new ConfluenceClientStub({
+      ...config,
+      baseUrl: new URL("https://confluence.example.com"),
+    });
+    expect(confluenceClient.BASE_URL).toBe("https://confluence.example.com");
+    expect(confluenceClient.API_V1_PATH).toBe("/wiki/rest/api");
+    expect(confluenceClient.API_V2_PATH).toBe("/wiki/api/v2");
+  });
+
+  it("should initialize the confluence client correctly with trailing slash", () => {
+    const confluenceClient = new ConfluenceClientStub({
+      ...config,
+      baseUrl: new URL("https://confluence.example.com/"),
+    });
+    expect(confluenceClient.BASE_URL).toBe("https://confluence.example.com");
+    expect(confluenceClient.API_V1_PATH).toBe("/wiki/rest/api");
+    expect(confluenceClient.API_V2_PATH).toBe("/wiki/api/v2");
+  });
+
+  it("should initialize the confluence client correctly with custom context", () => {
+    const confluenceClient = new ConfluenceClientStub({
+      ...config,
+      baseUrl: new URL("https://confluence.example.com/custom"),
+    });
+    expect(confluenceClient.BASE_URL).toBe("https://confluence.example.com");
+    expect(confluenceClient.API_V1_PATH).toBe("/custom/rest/api");
+    expect(confluenceClient.API_V2_PATH).toBe("/custom/api/v2");
+  });
+});
